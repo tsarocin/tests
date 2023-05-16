bff1f7ea9e2673aeb839bcb4e89c1820f2d57797
Nico <122835168+tsarocin@users.noreply.github.com>
2023-05-16 18:52:52 +0000

    new files

---
+++ Hello_World/build/CMakeFiles/tdd_functions_lib.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tdd_functions_lib.dir/src/functions.cpp.o"
+  "libtdd_functions_lib.a"
+  "libtdd_functions_lib.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/tdd_functions_lib.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Hello_World/build/CMakeFiles/tdd_functions_lib.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for tdd_functions_lib.
+# This may be replaced when dependencies are built.
