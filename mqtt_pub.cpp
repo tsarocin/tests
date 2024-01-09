47e8d36153efad57e1e9f058a451102fef0a1c3f
Nico <122835168+tsarocin@users.noreply.github.com>
2024-01-09 10:03:57 +0000

    mosquitto

---
+++ mqtt_pub.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <cstring>
+#include <mosquitto.h>
+
+int main() {
+    int rc;
+    struct mosquitto *mosq;
+
+    mosquitto_lib_init();
+    mosq = mosquitto_new("publisher-test", true, nullptr);
+
+    rc = mosquitto_connect(mosq, "localhost", 1883, 60);
+    if (rc != 0) {
+        std::cerr << "Client could not connect to broker! Error Code: " << rc << std::endl;
+        mosquitto_destroy(mosq);
+        mosquitto_lib_cleanup();
+        return -1;
+    }
+
+    std::cout << "We are now connected to the broker!" << std::endl;
+
+    const char* message = "Hello";
+    rc = mosquitto_publish(mosq, nullptr, "test/t1", std::strlen(message), message, 0, false);
+    if (rc != 0) {
+        std::cerr << "Error publishing message! Error Code: " << rc << std::endl;
+    }
+
+    mosquitto_disconnect(mosq);
+    mosquitto_destroy(mosq);
+    mosquitto_lib_cleanup();
+
+    return 0;
+}
