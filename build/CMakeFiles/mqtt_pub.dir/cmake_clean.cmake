c42ac36409f275e8039e57264178d811663ab6dd
Nico <122835168+tsarocin@users.noreply.github.com>
2024-01-10 15:55:17 +0000

    cmake

---
+++ build/CMakeFiles/mqtt_pub.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mqtt_pub.dir/mqtt_pub.cpp.o"
+  "CMakeFiles/mqtt_pub.dir/mqtt_pub.cpp.o.d"
+  "mqtt_pub"
+  "mqtt_pub.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/mqtt_pub.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
